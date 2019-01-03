3ee73cefe48a255ee85352697d0c803c58f6b829
unknown <jubayirhossain4448@gmail.com>
2019-01-03 23:14:04 +0600

    hire project

--- hotelbooking/src/main/java/com/jubayir/entity/Service.java
+++ hotelbooking/src/main/java/com/jubayir/entity/Service.java
@@ -10,5 +10,66 @@ public class Service {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "resturant")
+    private String resturant;
 
+    @Column(name = "swimmingPool")
+    private String swimmingPool;
+
+    @Column(name = "shopping_center")
+    private String shoppingCenter;
+
+    @Column(name = "business_center")
+    private String businessCenter;
+
+    @Column(name = "health_club")
+    private String healthClub;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getResturant() {
+        return resturant;
+    }
+
+    public void setResturant(String resturant) {
+        this.resturant = resturant;
+    }
+
+    public String getSwimmingPool() {
+        return swimmingPool;
+    }
+
+    public void setSwimmingPool(String swimmingPool) {
+        this.swimmingPool = swimmingPool;
+    }
+
+    public String getShoppingCenter() {
+        return shoppingCenter;
+    }
+
+    public void setShoppingCenter(String shoppingCenter) {
+        this.shoppingCenter = shoppingCenter;
+    }
+
+    public String getBusinessCenter() {
+        return businessCenter;
+    }
+
+    public void setBusinessCenter(String businessCenter) {
+        this.businessCenter = businessCenter;
+    }
+
+    public String getHealthClub() {
+        return healthClub;
+    }
+
+    public void setHealthClub(String healthClub) {
+        this.healthClub = healthClub;
+    }
 }
